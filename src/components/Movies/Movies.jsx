eac4c088ff7b73ebf08aaa123ffa0f2ca9e801ce
german <sorokin@deck.lc>
2021-12-07 17:28:41 +0300

    level-2

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
+import ToggleOn from "../../images/toggle-on.svg";
+import ToggleOff from "../../images/toggle-off.svg";
 
 const Movies = () => {
+
+    const [addShorts, setAddShorts] = useState(false)
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault()
+
+    }
+
     return <>
         <Header/>
         <section className={'movies section'}>
             <div className={'movies__content content'}>
                 <div className={'movies__search-container'}>
-                    <form className={'movies__form'}>
-                        <input type={'text'} className={'movies__input'} placeholder={'Фильм'}/>
-                        <input  type={'submit'} className={'movies__search-button button'} value={'Найти'}/>
-                        <input type={'checkbox'} className={'movies__toggle'}/>
+                    <form
+                        onSubmit={(e) => handleFormSubmit(e)}
+                        className={'movies__form'}>
+                        <input
+                            type={'text'}
+                            className={'movies__input'}
+                            placeholder={'Фильм'}/>
+                        <input
+                            type={'submit'}
+                            className={'movies__search-button button'}
+                            value={'Найти'}/>
+                        <div className={'movies__toggle-container'}>
+                            <img
+                                className={'movies__toggle button'}
+                                src={addShorts ? ToggleOn : ToggleOff}
+                                alt={'toggle'}
+                                onClick={() => setAddShorts(!addShorts)}/>
+                        </div>
+                        <span className={'movies__toggle-span'}>
+                            Короткометражки
+                        </span>
                     </form>
                     <MoviesCardList/>
                 </div>
