d9b957df51822f8f0e1c806f50866937281e340e
german <sorokin@deck.lc>
2021-12-20 18:05:19 +0300

    level-2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import Logo from '../../images/logo.svg'
 import Person from '../../images/person.svg'
 import {Link, useLocation} from "react-router-dom";
 
-const Header = () => {
+const Header = ({loggedIn}) => {
 
     const {pathname} = useLocation()
 
@@ -13,68 +13,74 @@ const Header = () => {
         <Link to={'/'} className={'header__logo-link'}>
             <img className={'header__logo button'} alt={'Logo'} src={Logo}/>
         </Link>
-        {pathname !== '/'
-        && <><ul className={menuOpened ? 'header__list header__list_first header__list_visible' : 'header__list header__list_first header__list_hidden'}>
-            <li className={'header__item'}>
-                <Link to={'/movies'}
-                      className={`header__link link button ${pathname === '/movies'
-                          ? 'header__link_active'
-                          : 'header__link_secondary'}`}>
-                    Фильмы
-                </Link>
-            </li>
-            <li className={'header__item'}>
-                <Link to={'/saved-movies'}
-                      className={`header__link link button ${pathname === '/saved-movies'
-                          ? 'header__link_active'
-                          : 'header__link_secondary'}`}>
-                    Сохранённые фильмы
-                </Link>
-            </li>
-        </ul>
-
-            </>}
-        <ul className={menuOpened ? 'header__list header__list-second header__list_visible' : 'header__list header__list_second header__list_hidden'}>
-            {pathname === '/saved-movies' || pathname === '/movies'
-                ? <li className={'header__item'}>
+        {loggedIn ? <>
+                <ul className={menuOpened
+                    ? 'header__list header__list_first header__list_visible'
+                    : 'header__list header__list_first header__list_hidden'}>
+                    <li className={'header__item'}>
+                        <Link to={'/movies'}
+                              className={`header__link link button ${pathname === '/movies'
+                                  ? 'header__link_active'
+                                  : 'header__link_secondary'}`}>
+                            Фильмы
+                        </Link>
+                    </li>
+                    <li className={'header__item'}>
+                        <Link to={'/saved-movies'}
+                              className={`header__link link button ${pathname === '/saved-movies'
+                                  ? 'header__link_active'
+                                  : 'header__link_secondary'}`}>
+                            Сохранённые фильмы
+                        </Link>
+                    </li>
+                </ul>
+                <ul className={menuOpened
+                    ? 'header__list header__list_first header__list_visible'
+                    : 'header__list header__list_first header__list_hidden'}>
+                    <li className={'header__item'}>
+                        <Link
+                            to={'/profile'}
+                            className={'header__link header__link_account link button'}>
+                            Аккаунт
+                            <img src={Person} alt={'Person'} className={'header__account-logo'}/>
+                        </Link>
+                    </li>
+                </ul>
+                <input
+                    className={'header__burger'}
+                    type={'checkbox'}
+                    id={'menu_checkbox'}
+                    checked={menuOpened}
+                    onChange={() => {
+                        setMenuOpened(!menuOpened)
+                        console.log(menuOpened)
+                    }}/>
+                <label htmlFor={'menu_checkbox'} className={'header__burger-label'}>
+                    <div/>
+                    <div/>
+                    <div/>
+                </label>
+            </> :
+            <ul className={'header__list header__list_second'}>
+                <li className={'header__item'}>
                     <Link
-                        to={'/profile'}
-                        className={'header__link header__link_account link button'}>
-                        Аккаунт
-                        <img src={Person} alt={'Person'} className={'header__account-logo'}/>
+                        to={'/signup'}
+                        className={'header__link link button'}>
+                        Регистрация
                     </Link>
                 </li>
-                : <>
-                    <li className={'header__item'}>
-                        <Link
-                            to={'/signup'}
-                            className={'header__link link button'}>
-                            Регистрация
-                        </Link>
-                    </li>
-                    <li className={'header__item'}>
-                        <Link
-                            to={'/signin'}
-                            className={'header__link header__link_primary link button'}>
-                            Войти
-                        </Link>
-                    </li>
-                </>}
-        </ul>
-        <input
-            className={'header__burger'}
-            type={'checkbox'}
-            id={'menu_checkbox'}
-            checked={menuOpened}
-            onChange={() => {
-                setMenuOpened(!menuOpened)
-                console.log(menuOpened)
-            }}/>
-        <label htmlFor={'menu_checkbox'} className={'header__burger-label'}>
-            <div/>
-            <div/>
-            <div/>
-        </label>
+                <li className={'header__item'}>
+                    <Link
+                        to={'/signin'}
+                        className={'header__link header__link_primary link button'}>
+                        Войти
+                    </Link>
+                </li>
+            </ul>}
+
+
+
+
     </nav>
 }
 export default Header
